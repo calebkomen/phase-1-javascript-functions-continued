23fe518a3eae86d0323c82b7a02dd3fa8198e774
CALEB KOMEN <komencaleb502@gmail.com>
2024-03-19 23:14:43 +0300

    mondaywork

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(name='roller-skate'){
+    return `This Saturday, I want to ${name}!`
+}
+console.log(saturdayFun())
+
+function mondayWork(work= 'go to the office'){
+    return `This Monday, I will ${work}.`
+}
+console.log(mondayWork())
+
+function wrapAdjective(visual="*"){
+    return function(Komen="special"){
+        return `You are ${visual}${Komen}${visual}!`
+    }
+    
+}
